ac7f2ed4ef2bdce84176326d142931b749d1bce8
Rein-Vanbelleghem <rein.vanbelleghem@skyline.be>
2024-04-04 19:47:56 +0200

    changed the python script and pipeline

--- add_additional_info.py
+++ add_additional_info.py
@@ -1,35 +1,44 @@
+'''
+This script will add a new version to the site.yaml if there is a new version inside the index.yaml
+and will copy the site.yaml from the chart to the corresponding new verison inside 
+the site.yaml in the root directory.
+'''
+
 import yaml
 import os
 import sys
 
-def merge_site_into_index(charts_dir, index_file_path):
+def update_site_with_new_version(charts_dir, index_file_path, site_file_path):
     with open(index_file_path, 'r') as index_file:
         index_data = yaml.safe_load(index_file)
 
-    for chart_name in index_data['entries']:
-        site_file_path = os.path.join(charts_dir, chart_name, 'site.yaml')
-        
-        if os.path.exists(site_file_path):
-            with open(site_file_path, 'r') as site_file:
-                site_data = yaml.safe_load(site_file)
-            
-            for version_entry in index_data['entries'][chart_name]:
-                try:
-                    version_entry.update(site_data[version_entry['version']])
-                except KeyError:
-                    try:
-                        version_entry.update(site_data['default'])
-                    except KeyError:
-                        pass
+    if os.path.exists(site_file_path):
+        with open(site_file_path, 'r') as site_file:
+            site_data = yaml.safe_load(site_file) or {'entries': {}}
+    else:
+        site_data = {'entries': {}}
 
-    # Write the updated index data back to the index.yaml file
-    with open(index_file_path, 'w') as index_file:
-        yaml.dump(index_data, index_file, default_flow_style=False)
+    for chart_name, versions in index_data['entries'].items():
+        for version_info in versions:
+            version = version_info['version']
+            print(f"Processing {chart_name}-{version}")
+            application_site_file_path = os.path.join(charts_dir, chart_name, 'site.yaml')
+            if os.path.exists(application_site_file_path):
+                with open(application_site_file_path, 'r') as app_site_file:
+                    app_site_data = yaml.safe_load(app_site_file)
+                    
+                    if chart_name not in site_data['entries']:
+                        site_data['entries'][chart_name] = {}
+                    
+                    if version not in site_data['entries'][chart_name]:
+                        site_data['entries'][chart_name][version] = app_site_data
+                
+    with open(site_file_path, 'w') as site_file:
+        yaml.dump(site_data, site_file, default_flow_style=False)
 
 if __name__ == "__main__":
-    # Retrieve the paths from environment variables or script arguments
     charts_dir = os.getenv('CHARTS_DIR', sys.argv[1] if len(sys.argv) > 1 else './charts')
-    index_file_path = os.getenv('SITE_FILE', sys.argv[2] if len(sys.argv) > 2 else './site.yaml')
+    index_file_path = os.getenv('INDEX_FILE', sys.argv[2] if len(sys.argv) > 2 else './index.yaml')
+    site_file_path = os.getenv('SITE_FILE', sys.argv[3] if len(sys.argv) > 3 else './site.yaml')
 
-    # Execute the merge function
-    merge_site_into_index(charts_dir, index_file_path)
+    update_site_with_new_version(charts_dir, index_file_path, site_file_path)
